a58f953bdaf3e1ced44130284b1ed8a508c88ee6
MatiasChiprut <92793353+MatiasCHiprut@users.noreply.github.com>
2022-04-04 11:32:30 -0300

    Initial commit

---
+++ Assets/Scripts/ej2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+        int res = num1 + num2;
+        int res2 = num1 * num2;
+        Debug.Log(res);
+        Debug.Log(res2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ej6.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej6 : MonoBehaviour
+{
+    public int num1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 % 2 == 0)
+        {
+            Debug.Log("El número es par");
+        }
+        else
+        {
+            Debug.Log("El número es impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
